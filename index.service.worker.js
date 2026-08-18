4ebe958617415fef73226f7f84dd88f10d500ebd
KDB-Mix <kareemdb3+github@gmail.com>
2026-08-18 13:42:47 +0400

    Features and fixes

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,9 +4,9 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1786985737|408109270';
+const CACHE_VERSION = '1787045990|9644982823';
 /** @type {string} */
-const CACHE_PREFIX = 'Flappy bird-sw-cache-';
+const CACHE_PREFIX = 'Bouncy Duck-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 /** @type {string} */
 const OFFLINE_URL = 'index.offline.html';
